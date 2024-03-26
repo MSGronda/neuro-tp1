5f6feb41ce4c51170210e3f1295a8fe1c0b2b629
MSGronda <mgronda@itba.edu.ar>
2024-03-26 16:15:06 -0300

    feat: started ej2

---
+++ ej2/main.py
@@ -0,0 +1,9 @@
+from utils import *
+
+
+if __name__ == "__main__":
+    dataset = get_signal_data('./dataset', SOLVING_ARITHMATIC)
+
+
+
+
---
+++ ej2/utils.py
@@ -0,0 +1,25 @@
+import os
+import numpy as np
+import pyedflib
+
+
+RELAXED = '_1.edf'
+SOLVING_ARITHMATIC = '_2.edf'
+
+
+def get_signal_data(path: str, ends_with: str):
+    data = []
+    for filename in os.listdir(path):
+        if filename.endswith(ends_with):
+            d = pyedflib.EdfReader(os.path.join(path, filename))
+
+            patient_data = []
+            for i in range(d.signals_in_file):
+                patient_data.append(d.readSignal(i))
+
+            d.close()
+
+            data.append(np.array(patient_data))
+
+    return data
+
